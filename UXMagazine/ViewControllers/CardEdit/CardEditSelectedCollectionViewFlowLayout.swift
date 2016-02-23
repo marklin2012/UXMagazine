bd7b87eb28bdfc47b0d03161a6e0cc2e95d35775
linyi31 <linyi@jd.com>
2016-02-23 16:04:33 +0800

    Fix: collectionView pageed

--- UXMagazine/ViewControllers/CardEdit/CardEditSelectedCollectionViewFlowLayout.swift
+++ UXMagazine/ViewControllers/CardEdit/CardEditSelectedCollectionViewFlowLayout.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class CardEditSelectedCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
+    private var inset: CGFloat?
+    
+    override func prepareLayout() {
+        super.prepareLayout()
+        // 设置内边距
+        inset = (collectionView!.frame.size.width - itemSize.width)/2
+        sectionInset = UIEdgeInsets(top: 0, left: inset!, bottom: 0, right: inset!)
+    }
+    
     override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
         return true
     }
@@ -25,15 +34,34 @@ class CardEditSelectedCollectionViewFlowLayout: UICollectionViewFlowLayout {
         
         for attribute  in attributes! {
             let delta = fabs(attribute.center.x - centerX)
-                
-                print(50 * (delta/100))
-                
+            
                 attribute.center.y = centerY - 10 * (1 - delta/100)
             
         } // for attribute  in attributes!
         
         return attributes
         
+    }
+    
+    override func targetContentOffsetForProposedContentOffset(var proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        
+        // 按页翻动
+        let rect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView!.frame.size.width, height: collectionView!.frame.size.height)
+        
+        let attrs = super.layoutAttributesForElementsInRect(rect)
+        
+        let centerX = proposedContentOffset.x + (collectionView?.frame.size.width)! / 2
+        
+        var minDelta = MAXFLOAT
+        
+        for attr in attrs! {
+            if fabs(Double(minDelta)) > fabs(Double(attr.center.x - centerX)) {
+                minDelta = Float(attr.center.x - centerX)
+            }
+        }
+        
+        proposedContentOffset.x += CGFloat(minDelta)
+        return proposedContentOffset
     }
     
     
